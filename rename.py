9a82f1ac55c7bd026a2580d018bf67d5a4b75ca3
Godzilla <onionparsley914@gmail.com>
2024-08-25 21:16:23 +0800

    modify: use re to match file name

--- rename.py
+++ rename.py
@@ -5,12 +5,8 @@ import tkinter.filedialog as tkfd
 import tkinter.messagebox as tm
 import re
 
-def getDirector():
+def getDirector(ref_file_dir):
     system_separate=os.sep
-    file_dir=path_input_frame.get()
-    if file_dir=='':
-        tm.showerror("Error","文件选择路径不能为空！")
-        return
     try:
         for out_root,dirs,files in os.walk(file_dir):
             if len(files) == 0:
@@ -26,9 +22,9 @@ def getDirector():
                         sub_files=set[2]
                         # 循环遍历子文件夹的文件数组
                         for file_index in range(len(sub_files)):
-                            if sub_files[file_index].count('-')>1 or sub_files[file_index].count('_') > 1:
-                                tm.showerror('Error','该文件命名格式有误：'+root+system_separate+sub_files[file_index])
-                                return
+           #                if sub_files[file_index].count('-')>1 or sub_files[file_index].count('_') > 1:
+           #                    tm.showerror('Error','该文件命名格式有误：'+root+system_separate+sub_files[file_index])
+           #                    return
                             # 完整的旧文件路径
                             old_name_complete=root+system_separate+sub_files[file_index]
                             # 旧文件名
@@ -36,30 +32,40 @@ def getDirector():
                             # 旧文件名后缀格式
                             old_name_suffix=sub_files[file_index].split(".")[1]
                             # 用-或_分割文件名
-                            after_split=re.split('-|_',old_name)
-                            pattern = re.compile(r'[A-Za-z]\d{1,4}')
+           #                after_split=re.split('-|_',old_name)
+                            hans_name_match=re.findall('[\u4e00-\u9fa5]{1,4}',old_name)
+                            employee_number_match=re.findall('[A-Za-z]\d{1,4}',old_name)
+                            new_file_name=''
+                            if (len(hans_name_match)>0) & (len(employee_number_match))>0:
+                                new_file_name=hans_name_match[0]+'_'+str(employee_number_match[0]).upper()
+                            else:
+                                tm.showerror('Error','该文件命名缺少姓名或工号：'+root+system_separate+sub_files[file_index])
+                                return
+                            if new_file_name!='':
+                                complete_new_file_name=root+system_separate+new_file_name+'.'+old_name_suffix
+                                os.rename(old_name_complete,complete_new_file_name)
                             # 如果符合姓名-工号的格式则进方法体
-                            if pattern.match(after_split[0]) == None:
-                                # 如果文件名包含-符号，则跳过
-                                if old_name.count('-')==1:
-                                    continue
-                                elif old_name.count('_') ==1:
-                                # 如果文件名包含_符号，则替换成-
-                                    modify_file_name=root+system_separate+after_split[0]+'-'+after_split[1]+'.'+old_name_suffix
-                                    os.rename(old_name_complete,modify_file_name)
-                                    continue
-                            search_result = pattern.search(after_split[0]).group()
-                            if len(search_result) != 0:
-                                temp = after_split[1]
-                                after_split[1] = after_split[0]
-                                after_split[0] = temp
-                                concat_file_name=after_split[0]+"-"+after_split[1]
-                                print(concat_file_name)
-                                new_name=concat_file_name+"."+old_name_suffix
-                                new_name_complete=root+system_separate +new_name
-                                print(old_name_complete)
-                                print(new_name_complete)
-                                os.rename(old_name_complete,new_name_complete)
+  #                       if pattern.match(after_split[0]) == None:
+  #                           # 如果文件名包含-符号，则跳过
+  #                           if old_name.count('-')==1:
+  #                               continue
+  #                           elif old_name.count('_') ==1:
+  #                           # 如果文件名包含_符号，则替换成-
+  #                               modify_file_name=root+system_separate+after_split[0]+'-'+after_split[1]+'.'+old_name_suffix
+  #                               os.rename(old_name_complete,modify_file_name)
+  #                               continue
+  #                       search_result = pattern.search(after_split[0]).group()
+  #                       if len(search_result) != 0:
+  #                           temp = after_split[1]
+  #                           after_split[1] = after_split[0]
+  #                           after_split[0] = temp
+  #                           concat_file_name=after_split[0]+"-"+after_split[1]
+  #                           print(concat_file_name)
+  #                           new_name=concat_file_name+"."+old_name_suffix
+  #                           new_name_complete=root+system_separate +new_name
+  #                           print(old_name_complete)
+  #                           print(new_name_complete)
+  #                           os.rename(old_name_complete,new_name_complete)
             else:
                 print("外层文件夹没有文件"+out_root)
     except:
@@ -73,7 +79,14 @@ def selectFile():
     path_input_frame.insert(0,file_dir)
 
 def execute():
-    getDirector()
+    file_dir=path_input_frame.get()
+    if file_dir=='':
+        tm.showerror("Error","文件选择路径不能为空！")
+        return
+    if file_dir.count(os.sep):
+        tm.showerror("Error","文件路径不正确！")
+        return
+    getDirector(file_dir)
     pass
 
 def showWindow():
@@ -95,4 +108,3 @@ def showWindow():
 
 if __name__=="__main__":
     showWindow()
-    getDirector()
